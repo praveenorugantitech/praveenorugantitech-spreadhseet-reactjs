2b005e8444ff81516a2fc2df0f65f8c36c249827
Praveen Oruganti <praveenorugantitech@gmail.com>
2021-02-19 20:51:09 +0530

    Google SpreadSheet

---
+++ README.md
@@ -0,0 +1,39 @@
+# Google SpreadSheet Update via UI developed using React JS and Sheet Best API
+
+### API
+
+[https://sheet.best/](https://sheet.best/) and create your free account.
+
+Please make sure to provide your Google SpreadSheet which you have shared with editor access link after creating a connection in [https://sheet.best/](https://sheet.best/).
+
+Also make sure you create the columns in Google SpreadSheet.
+
+
+![screenshot of the app](https://raw.githubusercontent.com/praveenorugantitech/praveenorugantitech-spreadhseet-reactjs/master/src/images/screenshot1.PNG)
+    
+
+### App Screenshots
+
+
+![screenshot of the app](https://raw.githubusercontent.com/praveenorugantitech/praveenorugantitech-spreadhseet-reactjs/master/src/images/screenshot2.PNG)
+
+Google Spreadhsset after Submitting the From
+
+![screenshot of the app](https://raw.githubusercontent.com/praveenorugantitech/praveenorugantitech-spreadhseet-reactjs/master/src/images/screenshot3.PNG)
+
+
+
+### Live Demo
+
+[https://praveenorugantitech.github.io/praveenorugantitech-spreadhseet-reactjs/](https://praveenorugantitech.github.io/praveenorugantitech-spreadhseet-reactjs/)
+
+
+### Setup Local Instructions
+
+```
+git clone https://github.com/praveenorugantitech/praveenorugantitech-spreadhseet-reactjs.git
+cd praveenorugantitech-spreadhseet-reactjs
+npm install
+npm start
+
+```
---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+import { Button, Form, Container, Header } from 'semantic-ui-react'
+import './App.css';
+import axios from 'axios';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      FirstName: '',
+      LastName: '',
+      Email: '',
+      Website: ''
+    }
+  }
+
+  changeHandler = (e) => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  submitHandler = e => {
+    e.preventDefault();
+    axios.post('https://sheet.best/api/sheets/bfe1d9b8-0856-4057-9527-116533b75c2d', this.state);
+
+  }
+
+  render() {
+    const { FirstName, LastName, Email, Website } = this.state;
+    return (
+      <Container fluid className="container">
+        <Header as='h2'>React Google Spreadsheet!</Header>
+        <Form className="form" onSubmit={this.submitHandler}>
+          <Form.Field>
+            <label>First Name</label>
+            <input placeholder='Enter your First Name' type="text" name="FirstName" value={FirstName} onChange={this.changeHandler} />
+          </Form.Field>
+          <Form.Field>
+            <label>Last Name</label>
+            <input placeholder='Enter your Last Name' type="text" name="LastName" value={LastName} onChange={this.changeHandler} />
+          </Form.Field>
+          <Form.Field>
+            <label>Email</label>
+            <input placeholder='Enter your Email' type="email" name="Email" value={Email} onChange={this.changeHandler} />
+          </Form.Field>
+          <Form.Field>
+            <label>Website</label>
+            <input placeholder='Enter your Website' type="text" name="Website" value={Website} onChange={this.changeHandler} />
+          </Form.Field>
+
+          <Button color="blue" type='submit'>Submit</Button>
+        </Form>
+      </Container>
+    )
+  }
+}
